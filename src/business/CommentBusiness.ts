d5b361985e0c8a0eb2a036de2815532b45660864
VieiraMiguel <peixotovieira.miguel@gmail.com>
2023-05-12 12:45:52 -0300

    DataBase refatorado e atualizado faltando ajustar

---
+++ src/business/CommentBusiness.ts
@@ -0,0 +1,248 @@
+import { CommentDatabase } from "../database/CommentDatabase";
+import { CreateCommentInputDTO, CreateCommentOutputDTO } from "../dtos/comments/createComment.dto";
+import { DeleteCommentInputDTO, DeleteCommentOutputDTO } from "../dtos/comments/deleteComment.dto";
+import { EditCommentInputDTO, EditCommentOutputDTO } from "../dtos/comments/editComment.dto";
+import { GetCommentsInputDTO, GetCommentsOutputDTO } from "../dtos/comments/getComments.dto";
+import { LikeOrDislikeCommentInputDTO, LikeOrDislikeCommentOutputDTO } from "../dtos/comments/likeOrDislikeComment.dto";
+import { BadRequestError } from "../errors/BadRequestError";
+import { ForbiddenError } from "../errors/ForbiddenError";
+import { NotFoundError } from "../errors/NotFoundError";
+import { UnauthorizedError } from "../errors/UnauthorizedError";
+import { Comment } from "../models/Comment";
+import { POST_LIKE } from "../models/Post";
+import { LikeDislikeDB } from "../models/Comment";
+import { Post } from "../models/Post";
+import { USER_ROLES } from "../models/User";
+import { IdGenerator } from "../services/IdGenerator";
+import { TokenManager } from "../services/TokenManager";
+import { COMMENT_LIKE } from "../models/Comment";
+
+
+export class CommentBusiness {
+
+    constructor(
+        private commentsDatabase: CommentDatabase,
+        private idGenerator: IdGenerator,
+        private tokenManager: TokenManager
+    ) { }
+
+    public createComment = async (input: CreateCommentInputDTO): Promise<CreateCommentOutputDTO> => {
+
+        const { content, token } = input
+
+        const payload = this.tokenManager.getPayload(token);
+
+        if (token === undefined) {
+            throw new BadRequestError("'token' ausente");
+        }
+
+        if (!payload) {
+            throw new UnauthorizedError();
+        }
+
+        const id = this.idGenerator.generate();
+
+        const comment = new Comment(
+            id,
+            content,
+            0,
+            0,
+            new Date().toISOString(),
+            new Date().toISOString(),
+            payload.id,
+            payload.name
+        )
+
+        const commentDB = comment.toDBModel()
+
+        await this.commentsDatabase.createComment(commentDB)
+
+        const output: CreateCommentOutputDTO = undefined
+
+        return output
+    }
+    public getComments = async (input: GetCommentsInputDTO): Promise<GetCommentsOutputDTO> => {
+
+        const { token } = input
+
+        const payload = this.tokenManager.getPayload(token)
+
+        if (token === undefined) {
+            throw new BadRequestError("'token' ausente")
+        }
+
+        if (payload === null) {
+            throw new UnauthorizedError()
+        }
+
+        const commentsDBwithCreatorName =
+            await this.commentsDatabase.getCommentWithCreatorName()
+
+        const comments = commentsDBwithCreatorName
+            .map((commentWithCreatorName) => {
+                const comment = new Comment(
+                    commentWithCreatorName.id,
+                    commentWithCreatorName.content,
+                    commentWithCreatorName.likes,
+                    commentWithCreatorName.dislikes,
+                    commentWithCreatorName.created_at,
+                    commentWithCreatorName.updated_at,
+                    commentWithCreatorName.creator_id,
+                    commentWithCreatorName.creator_name
+                )
+
+                return comment.toBusinessModel()
+            })
+
+        const output: GetCommentsOutputDTO = comments
+
+        return output
+    }
+
+    public editComment = async (input: EditCommentInputDTO): Promise<EditCommentOutputDTO> => {
+
+        const { content, token, idToEdit } = input
+
+        const payload = this.tokenManager.getPayload(token)
+
+        if (!payload) {
+
+            throw new UnauthorizedError()
+        }
+
+        const commentDB = await this.commentsDatabase.getCommentById(idToEdit)
+
+        if (!commentDB) {
+
+            throw new NotFoundError('')
+        }
+
+        if (payload.id !== commentDB.creator_id) {
+
+            throw new ForbiddenError('')
+        }
+
+        const comment = new Comment(
+            commentDB.id,
+            commentDB.content,
+            commentDB.likes,
+            commentDB.dislikes,
+            commentDB.created_at,
+            new Date().toISOString(),
+            commentDB.creator_id,
+            payload.name
+        )
+
+        comment.setContent(content)
+
+        const editedCommentDB = comment.toDBModel()
+        await this.commentsDatabase.editComment(editedCommentDB)
+
+        const output: EditCommentOutputDTO = undefined
+
+        return output
+    }
+
+    public deleteComment = async (
+        input: DeleteCommentInputDTO
+    ): Promise<DeleteCommentOutputDTO> => {
+        const { token, idToDelete } = input
+
+        const payload = this.tokenManager.getPayload(token)
+
+        if (!payload) {
+            throw new UnauthorizedError()
+        }
+
+        const commentDB = await this.commentsDatabase
+            .getCommentById(idToDelete)
+
+        if (!commentDB) {
+            throw new NotFoundError("comment com essa id não existe")
+        }
+
+        if (payload.role !== USER_ROLES.ADMIN) {
+            if (payload.id !== commentDB.creator_id) {
+                throw new ForbiddenError("somente quem criou a comment pode editá-lo")
+            }
+        }
+
+        await this.commentsDatabase.deleteComment(idToDelete)
+
+        const output: DeleteCommentOutputDTO = undefined
+
+        return output
+    }
+
+    public async likeOrDislikeComment(input: LikeOrDislikeCommentInputDTO): Promise<LikeOrDislikeCommentOutputDTO> {
+
+        const { token, like, commentId } = input
+
+        const payload = this.tokenManager.getPayload(token)
+
+        if (!payload) {
+            throw new UnauthorizedError()
+        }
+
+        const commentDBWithCreatorName =
+            await this.commentsDatabase.findCommentWithCreatorNameById(commentId)
+
+        if (!commentDBWithCreatorName) {
+            throw new NotFoundError("comment com essa id não existe")
+        }
+
+        const comment = new Comment(
+            commentDBWithCreatorName.id,
+            commentDBWithCreatorName.content,
+            commentDBWithCreatorName.likes,
+            commentDBWithCreatorName.dislikes,
+            commentDBWithCreatorName.created_at,
+            commentDBWithCreatorName.updated_at,
+            commentDBWithCreatorName.creator_id,
+            commentDBWithCreatorName.creator_name
+        )
+
+        const likeSQlite = like ? 1 : 0
+
+        const likeDislikeDB: LikeDislikeDB = {
+            user_id: payload.id,
+            comment_id: commentId,
+            like: likeSQlite
+        }
+
+        const likeDislikeExists =
+            await this.commentsDatabase.findLikeDislike(likeDislikeDB)
+
+        if (likeDislikeExists === COMMENT_LIKE.ALREADY_LIKED) {
+            if (like) {
+                await this.commentsDatabase.removeLikeDislike(likeDislikeDB)
+                comment.removeLike()
+            } else {
+                await this.commentsDatabase.updateLikeDislike(likeDislikeDB)
+                comment.removeLike()
+                comment.addDislike()
+            }
+
+        } else if (likeDislikeExists === COMMENT_LIKE.ALREADY_DISLIKED) {
+            if (like === false) {
+                await this.commentsDatabase.removeLikeDislike(likeDislikeDB)
+                comment.removeDislike()
+            } else {
+                await this.commentsDatabase.updateLikeDislike(likeDislikeDB)
+                comment.removeDislike()
+                comment.addLike()
+            }
+
+        } else {
+            await this.commentsDatabase.insertLikeDislike(likeDislikeDB)
+            like ? comment.addLike() : comment.addDislike()
+        }
+
+        const editedCommentDB = comment.toDBModel()
+        await this.commentsDatabase.editComment(editedCommentDB)
+
+        const output: LikeOrDislikeCommentOutputDTO = undefined
+
+        return output
+    }
+}
---
+++ src/database/labeddit.sql
@@ -0,0 +1,134 @@
+-- Active: 1683423095092@@127.0.0.1@3306
+
+CREATE TABLE
+    users (
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        name TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role TEXT NOT NULL,
+        created_at TEXT DEFAULT (DATETIME()) NOT NULL
+    );
+
+INSERT INTO
+    users(
+        id,
+        name,
+        email,
+        password,
+        role
+    )
+VALUES (
+        'u001',
+        'admin',
+        'admin@email.com',
+        '$2a$12$DJuloF8t8aV4rQ0.w9fjK.tccdxAP1U0O558WMvlwC5KGaP4F7ABG',
+        --adminpass123
+        'ADMIN'
+    ), (
+        'u002',
+        'Link',
+        'hero_of_time@sheikahmail.com',
+        '$2a$12$bdGa7r6SOY36k43MuyMk/unvM/rAdEJu23bxw4pUvupSE1IURNE3u',
+        --linkpass123
+        'NORMAL'
+    ), (
+        'u003',
+        'Zelda',
+        'hyrule_princess@hyliamail.com',
+        '$2a$12$WKLuNxV3WAdn7XAVxpuv2utZSfp8tM0b16GLCTt0UXN///x.0LkZi',
+        --zeldapass123
+        'NORMAL'
+    ), (
+        'u004',
+        'Solid Snake',
+        'fox.hound_best@otacon.com',
+        '$2a$12$1vDgZn1bK74ybKjRkwJEHu.h36SkxR7Y.i2Q/FzFU9K77PQ2xmgLS',
+        --snakepass123
+        'NORMAL'
+    ), (
+        'u005',
+        'Big Boss',
+        'legendary_soldier@outterheaven.com',
+        '$2a$12$IU4JcbaE3N9Vh1Jce9rgSOYOxBoAYe93sgKsc1ahmrXtqXLzR6fHO',
+        --davidpass123
+        'NORMAL'
+    ), (
+        'u006',
+        'Samus Aran',
+        'galactic_bounty.hunter@federationmail.com',
+        '$2a$12$n0Bkb/Wl3sSUEhFuBQkhfeqjEBxbTInDohBD8xQyajKRwXW/svKGK',
+        --metroidpass123
+        'NORMAL'
+    );
+
+CREATE TABLE
+    posts(
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        creator_id TEXT NOT NULL,
+        content TEXT NOT NULL,
+        likes INTEGER DEFAULT(0) NOT NULL,
+        dislikes INTEGER DEFAULT(0) NOT NULL,
+        comments INTEGER DEFAULT (0) NOT NULL,
+        created_at TEXT DEFAULT (DATETIME()) NOT NULL,
+        updated_at TEXT DEFAULT (DATETIME()) NOT NULL,
+        FOREIGN KEY (creator_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
+    );
+
+INSERT INTO
+    posts(
+        id,
+        creator_id,
+        content,
+        likes,
+        dislikes
+    )
+VALUES (
+        'p001',
+        'u001',
+        'Labeddit is ON!',
+        5,
+        0
+    );
+
+CREATE TABLE
+    likes_dislikes_post(
+        user_id TEXT NOT NULL,
+        post_id TEXT NOT NULL,
+        like INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    comments(
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        post_id TEXT NOT NULL,
+        creator_id TEXT NOT NULL,
+        content TEXT NOT NULL,
+        likes INTEGER DEFAULT(0) NOT NULL,
+        dislikes INTEGER DEFAULT(0) NOT NULL,
+        created_at TEXT DEFAULT (DATETIME()) NOT NULL,
+        updated_at TEXT DEFAULT (DATETIME()) NOT NULL,
+        FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (creator_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    likes_dislikes_comment(
+        user_id TEXT NOT NULL,
+        comment_id TEXT NOT NULL,
+        like INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (comment_id) REFERENCES comments(id) ON UPDATE CASCADE ON DELETE CASCADE
+    );
+
+SELECT * FROM posts;
+
+SELECT * FROM users;
+
+DROP TABLE likes_dislikes;
+
+DROP TABLE users;
+
+DROP TABLE posts;
